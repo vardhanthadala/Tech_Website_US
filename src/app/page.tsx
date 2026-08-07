42cc468db19d35d3e7a2731e68ee18bbb0e3725e
Vardhan <thadalavardhangoud@gmail.com>
2026-08-07 16:09:43 +0530

    Remove all components from project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-slate-900 flex flex-col font-sans selection:bg-orange-500 selection:text-white">
-      <Navbar />
-      <main className="flex-grow">
-        <Hero />
-      </main>
-      <Footer />
-    </div>
+    <main className="min-h-screen bg-white" />
   );
 }
